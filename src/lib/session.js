aeaee014660ee16796a3be172b2e82a65e291669
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-27 00:04:31 +0500

    Add JWT

---
+++ src/lib/session.js
@@ -0,0 +1,50 @@
+import { cookies } from "next/headers";
+import * as jose from "jose";
+
+SESSION_KEY = "D682B3E5D47AAF666F1CE5492AF4E";
+
+const secret = jose.base64url.decode(SESSION_KEY);
+
+const issuer = "urn:ahmx:issuer";
+
+const audience = "urn:ahmx:audience";
+
+const expiresAt = "2h";
+
+export const encodeUserSession = async (userId) => {
+
+    const jwt = await new jose.EncryptJWT({ "user": userId })
+        .setProtectedHeader({ alg: "dir", enc: "A128CBC-HS256" })
+        .setIssuedAt()
+        .setIssuer(issuer)
+        .setAudience(audience)
+        .setExpirationTime(expiresAt)
+        .encrypt(secret);
+
+    return jwt;
+};
+
+export const decodeUserSession = async (jwt) => {
+
+    const { payload } = await jose.jwtDecrypt(jwt, secret, {
+        issuer: issuer,
+        audience: audience
+    });
+
+    return payload;
+};
+
+async function verifySession() {
+
+    const userId = "16788";
+
+    const jwtToken = await encodeUserSession(userId);
+
+    const payload = await decodeUserSession(jwtToken);
+
+    const { user } = payload;
+
+    console.log(user, payload, userId === user);
+};
+
+verifySession().then(x => console.log(x, " Verify")).catch(err => console.log(err));
