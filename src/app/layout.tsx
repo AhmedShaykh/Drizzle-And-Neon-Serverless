1ecc1dd4b1a024e72baf358f225d2f71ce25744c
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-31 14:07:48 +0500

    Initial Commit With Dark Mode Feature

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { ThemeProvider } from "@/Components/ThemeProvider";
+import { ModeToggle } from "@/Components/ModeToggle";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Drizzle And Neon Serverless App",
+  description: "Drizzle And Neon Serverless App"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <div className="absolute top-6 right-6">
+            <ModeToggle />
+          </div>
+        </ThemeProvider>
+      </body>
+    </html >
+  )
+};
