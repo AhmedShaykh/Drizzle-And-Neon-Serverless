06bd794771fa7063c74cb938bea30f1f10a2abbb
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-31 20:12:52 +0500

    Add Secret

---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import { NextAuthOptions, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { users } from "./schema";
+import { db } from "./db";
+import { eq } from "drizzle-orm";
+import { nanoid } from "nanoid";
+import "dotenv/config";
+
+export const authOptions: NextAuthOptions = {
+    adapter: DrizzleAdapter(db),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ],
+    pages: {
+        signIn: "/signin",
+    },
+};
