85b12c63cf48bf1e7028a309cd64cb5fea5e666d
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-02 16:09:29 +0500

    Final Commit

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         serverActions: true
     },
     env: {
-        DATABASE_URL: "postgres://ahmed.shaykhwork:ZYAod1Xn6NqE@ep-restless-frog-79495068-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+        DATABASE_URL: "postgres://ahmed.shaykhwork:<password>@ep-restless-frog-79495068-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
     },
     images: {
         domains: ['lh3.googleusercontent.com', "icon-library.com"]
